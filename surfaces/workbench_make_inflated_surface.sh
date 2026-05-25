b18650c735a63689ecd1d554c31765902dfaa851
cindyhfls <cindyhfls@gmail.com>
2026-05-25 13:45:22 -0400

    change iterations-scale to 1(default for small number of vertices

--- surfaces/workbench_make_inflated_surface.sh
+++ surfaces/workbench_make_inflated_surface.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 module load workbench
-surfname="onavg-ico32_rh"
+surfname="onavg-ico32_lh"
 wb_command -surface-generate-inflated \
     "$surfname"_midthickness.surf.gii \
     "$surfname"_inflated.surf.gii \
     "$surfname"_very_inflated.surf.gii \
-    -iterations-scale 2.5
+    -iterations-scale 1 
+    
+
+    # Generate inflated and very inflated surfaces. The output surfaces are
+    #   'matched' (have same XYZ range) to the anatomical surface. In most cases,
+    #   an iterations-scale of 1.0 (default) is sufficient.  However, if the
+    #   surface contains a large number of vertices (150,000), try an
+    #   iterations-scale of 2.5.
